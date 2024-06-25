9b574099b35df4fe473aa52699495addcee4a5d7
Jordan McKoy <jordan.bmp.mckoy@gmail.com>
2024-06-24 21:05:29 -0400

    Update MessageHandlerTest.java

--- src/test/java/com/tropicoss/guardian/networking/messaging/MessageHandlerTest.java
+++ src/test/java/com/tropicoss/guardian/networking/messaging/MessageHandlerTest.java
@@ -1,16 +1,27 @@
 package com.tropicoss.guardian.networking.messaging;
 
+import com.mojang.authlib.GameProfile;
 import com.tropicoss.guardian.config.ConfigurationManager;
 import com.tropicoss.guardian.discord.Bot;
+import com.tropicoss.guardian.utils.PlayerInfoFetcher;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
+import org.slf4j.Logger;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.UUID;
+import java.util.*;
+
+import static org.mockito.Mockito.*;
 
 public class MessageHandlerTest {
 
@@ -32,6 +43,8 @@ public class MessageHandlerTest {
     // Should not send player login message to discord if it is not a server
     @Test
     public void shouldNotSendLoginMessageToDiscord() throws InterruptedException, FileNotFoundException {
+
+        Logger logger = mock(Logger.class);
         // Uses Notch for Testing
         LoginMessage message = new LoginMessage("server", "069a79f4-44e9-4726-a5be-fca90e38aaf5");
 
@@ -40,7 +53,7 @@ public class MessageHandlerTest {
         InputStreamReader streamReader = new InputStreamReader(resourceAsStream);
 
         // instantiate the minecraft server in guardian
-        MinecraftServer minecraftServer = Mockito.mock(MinecraftServer.class);
+        MinecraftServer minecraftServer = mock(MinecraftServer.class);
 
         ConfigurationManager configurationManager = new ConfigurationManager(streamReader);
         configurationManager.loadConfig();
@@ -49,11 +62,25 @@ public class MessageHandlerTest {
         MessageHandler messageHandler = new MessageHandler(configurationManager, minecraftServer);
 
         // Create a mock bot object
-        Bot bot =  Mockito.mock(Bot.class);
+        Bot bot =  mock(Bot.class);
 
         messageHandler.setBot(bot);
 
+        ServerPlayerEntity serverPlayerEntity = new ServerPlayerEntity(minecraftServer, mock(ServerWorld.class), mock(GameProfile.class), null);
+        List<ServerPlayerEntity> serverPlayerEntityList = Collections.singletonList(serverPlayerEntity);;
+
+        PlayerManager playerManager = mock(PlayerManager.class);
+
+        when(minecraftServer.getPlayerManager()).thenReturn(playerManager);
+        when(minecraftServer.getPlayerManager().getPlayerList()).thenReturn(serverPlayerEntityList);
+
+        when(message.toConsoleString()).thenReturn("Login Message");
+
         // Call the Message Handler (LoginMessage)
         messageHandler.handleLoginMessage(message);
+
+        verify(logger).info("Login Message");
+        verify(serverPlayerEntity).sendMessage(any(Text.class), eq(false));
+        verify(bot).sendJoinMessage(any(PlayerInfoFetcher.Profile.class), anyString());
     }
 }
