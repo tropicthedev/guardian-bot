4edc699bd54a4c3f4df457da838a45f679156407
tropicthedev <jordan.bmp.mckoy@gmail.com>
2024-08-27 15:02:28 -0500

    Fixed images =/

--- client/src/routes/index.lazy.tsx
+++ client/src/routes/index.lazy.tsx
@@ -15,7 +15,7 @@ function Index() {
         <main className="w-full h-screen flex flex-col items-center justify-center px-4">
             <div className="max-w-sm w-full text-gray-600">
                 <div className="text-center">
-                    <img src="../src/assets/guardian.png" width={150} className="mx-auto rounded-full" />
+                    <img src="guardian.png" width={150} className="mx-auto rounded-full" />
                     <div className="mt-5 space-y-2">
                         <h3 className="text-white text-2xl font-bold sm:text-3xl">Welcome to Guardian</h3>
                     </div>
